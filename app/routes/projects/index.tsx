04dd8bb26c4ba5217be07442fe154fe0fa2e8e33
Mostafizur <mostafizzfuad@gmail.com>
2026-01-21 15:57:48 +0600

    feat: fetch projects with loader and add project type

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,8 +1,24 @@
-const ProjectsPage = () => {
+import type { Project } from "~/types";
+import type { Route } from "./+types";
+
+// Loader ফাংশন (সার্ভার সাইডে রান হবে)
+export async function loader({
+	request,
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
+	const res = await fetch("http://localhost:8000/projects");
+	const data = await res.json();
+	return { projects: data };
+}
+
+const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
+	const { projects } = loaderData as { projects: Project[] };
+	// console.log(projects);
+
 	return (
-		<section>
+		<>
 			<h2 className="text-3xl font-bold mb-8 text-white">🚀 Projects</h2>
-		</section>
+			{/* এখানে আমরা পরে প্রজেক্ট লিস্ট দেখাব */}
+		</>
 	);
 };
 export default ProjectsPage;
